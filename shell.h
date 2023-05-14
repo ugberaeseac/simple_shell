e89e45f07b30320a793f96fb99d97a7048b9435a
SouaOui <soumiaouzat@gmail.com>
2023-05-14 11:36:03 +0100

    Header File

--- shell.h
+++ shell.h
@@ -9,13 +9,14 @@
 #include <sys/types.h>
 #define PROMPT "$ "
 
-
 extern char **environ;
 
-char **tokenize_command(char *line, char *delem);
-int count_tokens(char *line, char *delem);
 char *_strcpy(char *dest, char *src);
 unsigned int _strlen(char *s);
 char *_strdup(char *src);
+void _prompt(char *lineptr, size_t size, int counter, char **argv);
+void _exit_d(char *lineptr);
+void _execve(char **linecmd, char *lineptr, int counter, char **argv);
+char **_parse_to_token(int num_token, char **lineptr, char *delim);
 
 #endif
