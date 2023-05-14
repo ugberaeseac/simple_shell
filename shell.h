251d0db2d2c19dea4cfed437e789761e4308b64b
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-05-14 15:12:19 -0700

    fixed function prototype conflict

--- shell.h
+++ shell.h
@@ -17,7 +17,7 @@ char *_strdup(char *src);
 void _prompt(char *lineptr, size_t size, int counter, char **argv);
 void _exit_d(char *lineptr);
 void _execve(char **linecmd, char *lineptr, int counter, char **argv);
-int calculate_tokens(char *lineptr, char *delim);
+int calculate_tokens(char *lineptr, const char *delim);
 char **_parse_to_token(int num_token, char *lineptr, const char *delim);
 
 #endif
