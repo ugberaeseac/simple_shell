e33c921ce57dc65ae023d07038b91cf8ac94dc2b
SouaOui <soumiaouzat@gmail.com>
2023-05-15 09:27:14 -0700

    updating Prototype

--- shell.h
+++ shell.h
@@ -14,10 +14,14 @@ extern char **environ;
 char *_strcpy(char *dest, char *src);
 unsigned int _strlen(char *s);
 char *_strdup(char *src);
+int _strcmp(char *s1, char *s2);
+int _strncmp(char *s1, char *s2, int n);
+char *_strcat(char *dest, char *src);
 void _prompt(char *lineptr, size_t size, int counter, char **argv);
 void _exit_d(char *lineptr);
 void _execve(char **linecmd, char *lineptr, int counter, char **argv);
-int calculate_tokens(char *lineptr, char *delim);
+int calculate_tokens(char *lineptr, const char *delim);
 char **_parse_to_token(int num_token, char *lineptr, const char *delim);
+char *path_value(void);
 
 #endif
