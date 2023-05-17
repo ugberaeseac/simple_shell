967a64ca5eb9a01979bee5c3fa345becbeea651e
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-05-17 11:32:06 -0700

    functionality to build the full executable command path

--- _path.c
+++ _path.c
@@ -12,7 +12,7 @@ char **_pathtokens(char *str)
 	char **tokens;
 	const char *delim = "\n:";
 
-	str = _getenv(PATH);
+	str = _getenv("PATH");
 	tokens = _parse_to_token(count, str, delim);
 	if (tokens == NULL)
 		return (NULL);
@@ -67,17 +67,31 @@ char *_exec_path(char **tokens, char *cmd)
 
 /**
  * _build_path - builds the executable path of the executable command
- * @cmd_path: full path of the executable command
+ * @e_path: full path of the executable command
  * @cmd: executable command
  *
  * Return: full path including executabe command
  */
-char *build_path(char *cmd_path, char *cmd)
+char *build_path(char *e_path, char *cmd)
 {
+	int path_length = 0;
+	char *path = NULL;
+
+	if (e_path == NULL || cmd== NULL)
+		return (NULL);
+
+	path_length = (_strlen(cmd_path) + _strlen(cmd) + 2);
+	path = malloc(sizeof(char) * path_length);
+	if (path == NULL)
+		return (NULL);
+
+	_strcpy(path, e_path);
+	_strcat(path, "/");
+	_strcat(path, cmd);
+	_strcat(path, "\0");
+
+	return (path);
 }
-
-
-
 
 
 /**
