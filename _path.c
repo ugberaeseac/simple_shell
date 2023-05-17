2c8a64799114c29989aa9e2de9bdc2d32e956e9e
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-05-17 10:53:26 -0700

    function to get the full path of the executable command

--- _path.c
+++ _path.c
@@ -2,7 +2,7 @@
 
 /**
  * _pathtokens - tokenize the environemtal variable PATH
- * @str: environmental variable string
+ * @str: PATH string
  *
  * Return: array of PATH tokens
  */
@@ -19,3 +19,50 @@ char **_pathtokens(char *str)
 
 	return (tokens);
 }
+
+
+/**
+ * _exec_path - get the full path of the executable command
+ * @cmd: executable command
+ * @tokens: array of PATH tokens
+ *
+ * Return: full path of executable file
+ */
+char *_exec_path(char **tokens, char *cmd)
+{
+	struct stat stat_buf;
+	char *pwd = NULL;
+	char *buf = NULL;
+	size_t size = 0;
+	int status, i = 0;
+
+	pwd = getcwd(buf, size);
+	if (pwd == NULL)
+		return (NULL);
+
+	if (cmd[0] == '\')
+		cmd = cmd + 1;
+	
+	for (i = 0; tokens[i] != NULL; i++)
+	{
+		status = chdir(tokens[i]);
+		if (status == -1)
+		{
+			perror("Error ");
+			return (NULL);
+		}
+		status = stat(cmd, stat_buf);
+		if (status == 0)
+		{
+			chdir(pwd);
+			free(pwd);
+			return (tokens[i]);
+		}
+	}
+	chdir(pwd);
+	free(pwd);
+	return (NULL);
+}
+
+
+
