4bd3b2fa9a31aa6e9c282672e8a3546c14b997bb
SouaOui <soumiaouzat@gmail.com>
2023-05-20 12:54:58 -0700

    Header File updated

--- shell.h
+++ shell.h
@@ -4,8 +4,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/wait.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #define PROMPT "$ "
 
@@ -33,12 +34,25 @@ void _exit_d(char *lineptr);
 void _execve(char **linecmd, char *lineptr, int counter, char **argv);
 int calculate_tokens(char *lineptr, const char *delim);
 char **_parse_to_token(int num_token, char *lineptr, const char *delim);
-char *path_value(void);
+char *_getenv(char *key);
 void (*_check_builtin(char *func))(char *str);
 void _env(char *lineptr);
 int _built_in(char **linecmd, char *lineptr);
 void _signal_ignore(int sig);
+void _free_double_ptr(char **str);
+void _exit_(char *lineptr);
+int _atoi(char *str);
+char **_pathtokens(char *str);
+char *_exec_path(char **tokens, char *cmd);
+char *_build_path(char *e_path, char *cmd);
+char *_getpath(char *cmd);
+int _putchar(char c);
+void _puts(char *str);
+int _print_number(int n);
+void _error_execve(char *argv, int counter, char *cmd);
+void _print_error(char *argv, int counter, char *command);
 char *update_add_env(char *key, char *value);
 int _setenv(char *key, char *value, int overwrite);
+
 
 #endif
