ea709f5901de4a1d2d730d9dcdd1daa4e406f600
SouaOui <soumiaouzat@gmail.com>
2023-05-23 03:21:22 -0700

    _strchr

--- _string2.c
+++ _string2.c
@@ -15,3 +15,19 @@ int _strcmp(char *s1, char *s2)
 		i++;
 	return (s1[i] - s2[i]);
 }
+/**
+ * _strchr - a function that locates a character in a string.
+ * @s: pointer to string
+ * @c: character to be located
+ * Return: returns the first occurrence of the character c
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != c)
+	{
+		if (*s == '\0')
+			return (NULL);
+		s++;
+	}
+	return (s);
+}
