7910a86b42baf7b9d4112b822665b37add18c643
SouaOui <soumiaouzat@gmail.com>
2023-05-26 10:58:07 -0700

    testing

--- _setenv.c
+++ _setenv.c
@@ -38,7 +38,8 @@ void _setenv(char *lineptr)
 	char *key_var;
 
 	linecmd = _parse_to_token(num_token, lineptr, delim);
-	if (linecmd[0] == NULL || linecmd[1] == NULL)
+	
+	if (linecmd[0] == NULL || linecmd[1] == NULL || linecmd[2] == NULL)
 	{
 		_puts("Too few arguments\n");
 		_free_double_ptr(linecmd);
